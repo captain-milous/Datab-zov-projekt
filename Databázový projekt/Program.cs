b0e82cb7ab4e252616c469f4216a0c909c007abb
captain-milous <milda.tesar3@gmail.com>
2023-02-25 15:32:17 +0100

    Add project files.

---
+++ Databázový projekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Databázový_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
