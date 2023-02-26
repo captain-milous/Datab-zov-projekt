4658fdd08f608d4fc8960114112911ddb07c128a
captain-milous <milda.tesar3@gmail.com>
2023-02-26 09:58:14 +0100

    User

---
+++ Databázový projekt/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databázový_projekt
+{
+    public class User
+    {
+        protected string email;
+        protected string heslo;
+        protected string adresa;
+
+        public string Email { get { return email; } set { email = value; } }
+        public string Heslo { get { return heslo; } set { heslo = value; } }
+        public string Adresa { get { return adresa; } set { adresa = value; } }
+
+        public User(string email, string heslo)
+        {
+            Email = email;
+            Heslo = heslo;
+            Adresa = "Neuvedena";
+        }
+
+        public User(string email, string heslo, string adresa)
+        {
+            Email = email;
+            Heslo = heslo;
+            Adresa = adresa;
+        }
+
+        public override string ToString()
+        {
+            return "Email: "+Email+", Heslo: "+Heslo;
+        }
+    }
+}
---
+++ Databázový projekt/Zakaznik.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databázový_projekt
+{
+    internal class Zakaznik : User
+    {
+        private string jmeno;
+        private string prijmeni;
+        private DateTime datumNarozeni;
+
+        public string Jmeno { get { return jmeno; } set { jmeno = value; } }
+        public string Prijmeni { get { return prijmeni; } set { prijmeni = value; } }
+        public DateTime DatumNarozeni { get { return datumNarozeni; } set { datumNarozeni = value; } }
+
+        public Zakaznik(string email, string heslo) : base(email, heslo)
+        {
+            Jmeno = "NoName";
+            Prijmeni = "NoSurname";
+            DatumNarozeni = DateTime.Today; 
+        }
+
+        public Zakaznik(string email, string heslo, string adresa, string jmeno, string prijmeni, DateTime datumNarozeni) : base(email, heslo, adresa)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            DatumNarozeni = datumNarozeni;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
